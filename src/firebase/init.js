2dc424631d44da94abd69db753c93a572cd5cf3f
Ana Afonso <ana.afonso.01@gmail.com>
2020-02-18 11:58:07 +0100

    remove

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,7 +3,7 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyDkYSqRkpphcqhmyCSKRGqNUjz7vF55S9M',
+  apiKey: '',
   authDomain: 'engagementkit.firebaseapp.com',
   databaseURL: 'https://engagementkit.firebaseio.com',
   projectId: 'engagementkit',
